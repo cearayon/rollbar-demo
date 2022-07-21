bd16adad8cbc25e9ac8fb529bcb8622934fc8f3e
Carlo Arayon <cearayon@gmail.com>
2022-07-21 14:33:35 -0700

    uh

--- server.js
+++ server.js
@@ -20,12 +20,6 @@ rollbar.log('Hello world!')
 const students = ['Jimmy', 'Timothy', 'Jimothy']
 
 app.get('/api/students', (req, res) => {
-    // try{
-    //     doesNotExist()
-    // } catch (error){
-    //     rollbar.info("Uh this function doesn't exist")
-    //     console.log(error)
-    // }
     rollbar.info('someone got the list of students to load')
     res.status(200).send(students)
 })
